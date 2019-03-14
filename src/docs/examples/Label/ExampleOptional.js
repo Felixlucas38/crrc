e7d2cd95715cea24c58feedebd4b65839a7f6bb2
Felix Lucas <felixlucas38@gmail.com>
2019-03-13 22:15:09 -0300

    Add ProgressBar component and examples

---
+++ src/docs/examples/Label/ExampleOptional.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Label from 'crrc/Label';
+
+/** Optional label */
+export default function ExampleOptional() {
+  return <Label htmlFor="optional" label="Optional Label" />;
+}
---
+++ src/docs/examples/Label/ExampleRequired.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Label from 'crrc/Label';
+
+/** Required label */
+export default function ExampleRequired() {
+  return <Label htmlFor="required" label="Required Label" required />;
+}
