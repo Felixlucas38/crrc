86da03a20f11bf9ecc4c68c95c5dd93fb4dee3b6
Felix Lucas <felixlucas38@gmail.com>
2019-03-21 00:08:50 -0300

    Interaction Test: Add interaction test to PasswordInput

--- src/components/PasswordInput/PasswordInput.spec.js
+++ src/components/PasswordInput/PasswordInput.spec.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import PasswordInput from './PasswordInput';
+import {shallow} from 'enzyme';
 
 describe('PasswordInput', () => {
+    test('toggles input type when show/hide password is clicked', () => {
+        const wrapper = shallow(<PasswordInput htmlId="test" name="test" value="test" onChange={() => {}} value="Uds2$$1f.0" showVisibilityToggle />);
+
+        // Password input should have a type of password initially
+        expect(wrapper.find({ type: 'password' })).toHaveLength(1);
+        expect(wrapper.find({ type: 'text' })).toHaveLength(0);
+
+        wrapper.find('button').simulate('click');
+
+        expect(wrapper.find({ type: 'password' })).toHaveLength(0);
+        expect(wrapper.find({ type: 'text' })).toHaveLength(1);
+    });
+
     test('hides password quality by default', () => {
         const tree = renderer.create(<PasswordInput htmlId="test" name="test" onChange={() => {}} value="Uds2$$1f.0" />).toJSON();
         expect(tree).toMatchSnapshot();
